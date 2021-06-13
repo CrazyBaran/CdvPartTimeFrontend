e3fb8d0eeafc9a6f62a1f7607ed226dbcec382e0
JAB <jakub.baranowski@ticos-systems.ch>
2021-06-13 11:19:38 +0200

    Initial

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,25 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'JakubPartTime',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44325',
+    redirectUri: baseUrl,
+    clientId: 'JakubPartTime_App',
+    responseType: 'code',
+    scope: 'offline_access JakubPartTime',
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44325',
+      rootNamespace: 'Cdv.JakubPartTime',
+    },
+  },
+} as Environment;
